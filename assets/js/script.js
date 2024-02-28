9ae6e21166b6811070cef3f8b49f14a71ed0d062
Razzoy <61683221+Razzoy@users.noreply.github.com>
2024-02-28 10:37:23 +0100

    done

---
+++ assets/js/script.js
@@ -0,0 +1,20 @@
+//LOCAL
+
+localStorage.setItem('name', 'Bob')
+// console.log(localStorage.getItem('name'))
+localStorage.removeItem('name')
+
+
+//SESSION
+
+sessionStorage.setItem('name', 'John')
+// console.log(sessionStorage.getItem('name'))
+sessionStorage.removeItem('name')
+sessionStorage.setItem('name', 'Bob')
+
+
+//COOKIES
+
+document.cookie = 'name=Kyle; expires=' + new Date(2026, 0, 1).toUTCString()
+
+document.cookie = 'lastName=Smith; expires=' + new Date(2026, 0, 1).toUTCString()
