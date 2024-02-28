d86ea25c3e1aa617835075e3a96f6d0e7f248830
Razzoy <61683221+Razzoy@users.noreply.github.com>
2024-02-28 10:54:59 +0100

    Extra

--- assets/js/script.js
+++ assets/js/script.js
@@ -18,3 +18,18 @@ sessionStorage.setItem('name', 'Bob')
 document.cookie = 'name=Kyle; expires=' + new Date(2026, 0, 1).toUTCString()
 
 document.cookie = 'lastName=Smith; expires=' + new Date(2026, 0, 1).toUTCString()
+
+
+let myObj = {
+    name: 'Domenic',
+    age: 56
+};
+
+let myObj_serialized = JSON.stringify(myObj);
+console.log(myObj_serialized);
+
+localStorage.setItem('myObj', myObj_serialized);
+
+let myObj_deserialized = JSON.parse(localStorage.getItem('myObj'));
+
+console.log(myObj_deserialized);
